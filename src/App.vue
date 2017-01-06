3114264f826494e8f77bd27abd78b589871a5c98
Jas0ncn <ceocjy@vip.qq.com>
2017-01-07 02:03:05 +0800

    config anything

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+  <div class="page entry">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
-
 export default {
-  name: 'app',
-  components: {
-    Hello
-  }
+  name: 'entry'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less">
+@import './assets/less/_reset.less';
+@import './assets/less/common.less';
+@import './assets/less/pages/entry.less';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import router from './router'
+import store from './vuex'
+
 import App from './App'
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  store,
+  router,
+  render: h => h(App)
 })
