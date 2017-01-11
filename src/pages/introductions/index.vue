2b6ebeb0d121e21ccf877716eb90f8800d5c53e0
Jas0ncn <ceocjy@vip.qq.com>
2017-01-12 02:02:59 +0800

    add introduction page

---
+++ src/pages/introductions/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page introductions">
+    <p>个人简介</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'introductions'
+}
+</script>
+
+<style lang="less" scoped>
+@import 'index.less';
+</style>
