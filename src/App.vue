30e94672c5a5ea71dba02445b1d0f8c82a8e0fac
Jas0ncn <ceocjy@vip.qq.com>
2017-01-12 02:02:44 +0800

    add navigator

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="page entry">
-    <router-view></router-view>
+    <navigator />
+    <transition :name="transitionName">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
 <script>
+import navigator from './components/navigator.vue'
+
 export default {
-  name: 'entry'
+  name: 'entry',
+  data: () => ({
+    transitionName: 'slideUpIn'
+  }),
+  watch: {
+    '$route' (to, from) {
+      this.transitionName = from.meta.weight && to.meta.weight < from.meta.weight ? 'slideDownIn' : 'slideUpIn'
+    }
+  },
+  components: {
+    navigator
+  }
 }
 </script>
 
@@ -14,4 +30,5 @@ export default {
 @import './assets/less/_reset.less';
 @import './assets/less/common.less';
 @import './assets/less/entry.less';
+@import './assets/less/animations.less';
 </style>
---
+++ src/components/navigator.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="container">
+    <div class="item" v-for="route in routeMap">
+      <router-link :to="route.path" class="link" :class="{ 'active-link': $route.path === '/' && route.path === 'index' }">
+        <img :src="`/static/svgs/navigator/${route.path}.svg`">
+      </router-link>
+      <span class="description">{{ route[$store.state.lang] }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { routeMap } from '../router'
+
+export default {
+  name: 'navigator',
+  data: () => ({
+    routeMap
+  })
+}
+</script>
+
+<style lang="less" scoped>
+.container {
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding-right: 40px;
+
+  height: 100%;
+  width: 200px;
+
+  pointer-events: none;
+
+  z-index: 20;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .item {
+    position: relative;
+    margin: 20px 0;
+    pointer-events: auto;
+
+    .link {
+      position: absolute;
+      right: 0;
+      top: 0;
+
+      width: 12px;
+      height: 12px;
+
+      background: rgba(0,0,0,.7);
+
+      transition: all 300ms ease;
+      border-radius: 50%;
+      opacity: .1;
+      cursor: pointer;
+
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      img {
+        width: 8px;
+        height: 8px;
+
+        opacity: 0;
+        transition: all 300ms ease;
+      }
+    }
+
+    .description {
+      position: absolute;
+      right: 40px;
+      top: -6px;
+      padding: 5px;
+      padding-left: 10px;
+      
+      color: #FFF;
+      font-size: 14px;
+      line-height: 14px;
+      font-weight: bold;
+      background: rgba(0,0,0,.7);
+      border-radius: 3px 1px 1px 3px;
+
+      transform: translateX(50px);
+      opacity: 0;
+      transition: all 600ms ease;
+
+      user-select: none;
+      cursor: pointer;
+
+      &:after {
+        position: absolute;
+        right: -24px;
+        top: 0;
+
+        width: 0;
+        height: 0;
+
+        border-left: 12px solid rgba(0,0,0,.7);
+        border-right: 12px solid transparent;
+        border-top: 12px solid transparent;
+        border-bottom: 12px solid transparent;
+
+        content: '';
+        display: block
+      }
+    }
+
+    .active-link {
+      transform: scale(3);
+      opacity: 1;
+
+      img {
+        opacity: 1;
+      }
+    }
+
+    &:hover {
+      .link {
+        transform: scale(3);
+        opacity: 1;
+
+        img {
+          opacity: 1;
+        }
+      }
+
+      .description {
+        transform: translateX(0);
+        opacity: 1;
+      }
+    }
+  }
+
+}
+</style>
