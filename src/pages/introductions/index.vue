886fe943c81d993e860c629f3873e320e9902cdd
Jas0ncn <ceocjy@vip.qq.com>
2017-02-09 14:53:49 +0800

    finish introductions page

--- src/pages/introductions/index.vue
+++ src/pages/introductions/index.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="page introductions">
     <h1 class="title">{{ $store.state.data.route[$route.meta.weight] }}</h1>
+    <div class="pagebody">
+      <div class="selfIntroduction">
+        <p>{{ $store.state.data.website.description }}</p>
+      </div>
+      <div class="selfInformations">
+        <div class="infoRow" v-for="index in Array(Math.ceil($store.state.data.website.infomations.length / 3)).fill(0).map((v, i) => i)">
+          <div class="placeholder"></div>
+          <div
+            class="infoItem"
+            v-for="(v, i) in $store.state.data.website.infomations"
+            v-if="i >= index * 3 && i < (index + 1) * 3">
+            <img :src="`/static/svgs/info/${v.icon}`" :alt="v.key" :title="v.key">
+            <p>{{ v.value }}</p>
+          </div>
+          <div class="placeholder"></div>
+        </div>
+    </div>
   </div>
 </template>
 
