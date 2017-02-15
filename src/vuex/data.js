2c9e0539b342e5b0bfdb6dad6057e3470f0f784c
Jas0ncn <ceocjy@vip.qq.com>
2017-02-15 20:52:27 +0800

    add skills data

--- src/vuex/data.js
+++ src/vuex/data.js
@@ -14,25 +14,70 @@ export default {
       shortDescription: '大三学生，前端工程师进阶中...',
       description: '大一开始我就负责学校多个微信公众号的开发和运营，并开始接触、关注开源社区和前端生态系统。',
       infomations: [{
-        icon: 'birthday.svg',
+        icon: '/static/svgs/info/birthday.svg',
         key: '生日',
         value: '1996.2.11'
       }, {
-        icon: 'education.svg',
+        icon: '/static/svgs/info/education.svg',
         key: '教育经历',
         value: '深圳大学 · 通信工程'
       }, {
-        icon: 'nowCity.svg',
+        icon: '/static/svgs/info/nowCity.svg',
         key: '居住地',
         value: '广东 · 深圳'
       }, {
-        icon: 'email.svg',
+        icon: '/static/svgs/info/email.svg',
         key: '邮箱',
         value: 'jason@iszu.cn'
       }, {
-        icon: 'phone.svg',
+        icon: '/static/svgs/info/phone.svg',
         key: '手机号',
         value: '+86 13128931074'
+      }],
+      skills: [{
+        value: 'HTML',
+        logo: '/static/images/logos/html.png'
+      }, {
+        value: 'CSS',
+        logo: '/static/images/logos/css.png'
+      }, {
+        value: 'JavaScript',
+        logo: '/static/images/logos/javascript.png',
+        style: {
+          background: 'linear-gradient(to bottom, #ffeb0e , #efdd1b)',
+          'border-color': '#ffeb0e'
+        }
+      }, {
+        value: 'Node.js',
+        logo: '/static/images/logos/nodejs.png',
+        style: {
+          background: 'linear-gradient(to bottom, #8ccf00 , #80bd01)',
+          'border-color': '#8ccf00'
+        }
+      }, {
+        value: 'PHP',
+        logo: '/static/images/logos/php.png',
+        style: {
+          background: 'linear-gradient(to bottom, #8093d1 , #7b8ec9)',
+          'border-color': '#8093d1'
+        }
+      }, {
+        value: 'Python',
+        logo: '/static/images/logos/python.png'
+      }, {
+        value: 'Photoshop',
+        logo: '/static/images/logos/photoshop.png',
+        style: {
+          background: '#001d26',
+          border: '5px solid #00c8ff'
+        }
+      }, {
+        value: 'Sketch',
+        logo: '/static/images/logos/sketch.png',
+        style: {
+          background: 'linear-gradient(to bottom, #1d0f4f , #130a33)',
+          'border-color': '#1d0f4f'
+        }
       }]
     },
     route: routeMap.map(v => v.cn)
@@ -63,6 +108,51 @@ export default {
         icon: 'phone.svg',
         key: 'Phone Number',
         value: '+86 13128931074'
+      }],
+      skills: [{
+        value: 'HTML',
+        logo: '/static/images/logos/html.png'
+      }, {
+        value: 'CSS',
+        logo: '/static/images/logos/css.png'
+      }, {
+        value: 'JavaScript',
+        logo: '/static/images/logos/javascript.png',
+        style: {
+          background: 'linear-gradient(to bottom, #ffeb0e , #efdd1b)',
+          'border-color': '#ffeb0e'
+        }
+      }, {
+        value: 'Node.js',
+        logo: '/static/images/logos/nodejs.png',
+        style: {
+          background: 'linear-gradient(to bottom, #8ccf00 , #80bd01)',
+          'border-color': '#8ccf00'
+        }
+      }, {
+        value: 'PHP',
+        logo: '/static/images/logos/php.png',
+        style: {
+          background: 'linear-gradient(to bottom, #8093d1 , #7b8ec9)',
+          'border-color': '#8093d1'
+        }
+      }, {
+        value: 'Python',
+        logo: '/static/images/logos/python.png'
+      }, {
+        value: 'Photoshop',
+        logo: '/static/images/logos/photoshop.png',
+        style: {
+          background: '#001d26',
+          border: '5px solid #00c8ff'
+        }
+      }, {
+        value: 'Sketch',
+        logo: '/static/images/logos/sketch.png',
+        style: {
+          background: 'linear-gradient(to bottom, #1d0f4f , #130a33)',
+          'border-color': '#1d0f4f'
+        }
       }]
     },
     route: routeMap.map(v => v.en)
