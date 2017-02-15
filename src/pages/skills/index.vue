584b956f5d78588f22c628bd72ef15ed17287ad8
Jas0ncn <ceocjy@vip.qq.com>
2017-02-15 20:51:24 +0800

    skills page finished

--- src/pages/skills/index.vue
+++ src/pages/skills/index.vue
@@ -3,7 +3,13 @@
     <h1 class="title">{{ $store.state.data.route[$route.meta.weight] }}</h1>
     <div class="pagebody">
       <div class="cardContainer">
-        
+        <div
+          class="card"
+          v-for="skill in $store.state.data.website.skills"
+          :style="skill.style ? skill.style : ''"
+          :data-desc="skill.value">
+          <img :src="skill.logo">
+        </div>
       </div>
     </div>
   </div>
